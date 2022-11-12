8d8080d62b322cda6c38c6030f1f49edddca31a4
nadine-gowaily-billy <113942538+nadine-gowaily-billy@users.noreply.github.com>
2022-11-12 20:32:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 document.addEventListener('click',function(){
- console.log("you've clicked the document!")});
+ const mainHeading= document.querySelector('h1');
+ mainHeading.style.backgroundColor='red';
+});
