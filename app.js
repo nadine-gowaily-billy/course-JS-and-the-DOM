bb6ddb05f4d1ab2cb655b993c1060657d4652a0b
nadine-gowaily-billy <113942538+nadine-gowaily-billy@users.noreply.github.com>
2022-11-12 21:51:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ document.addEventListener('click',function(){
  const mainHeading= document.querySelector('h1');
  mainHeading.style.backgroundColor='red';
 });
+document.body.addEventListener('Keypress',function(){
+ console.log('Removing First Child');
+ document.querySelector('#contain-all').firstElementChild.remove();
+});
